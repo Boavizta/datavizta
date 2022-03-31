143704441f8fcc0a6c454e0ee5f0767f4d2abc00
AirLoren <airloren@cmakers.io>
2022-03-31 16:08:30 +0200

    minor UI improvments

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,19 +5,19 @@
 
 <div id="navbar">
 	<img id="logo" src="./boavizta-logo-4.png" alt="Boavizta">
-	<h1 class="title-main">BOAVIZTA</h1>
+	<h1 class="title-main">Boavizta</h1>
 
 </div>
 <div id="content" class="px-4">
-	<h1 class="title-main">{$_('index.title')}</h1>
+	<h2 class="title-second">{$_('index.title')}</h2>
 	
 	<div class="">
 		<div class="mb-3">
-			<p class="mb-2">
+			<p class="text-sm mb-2">
 				{$_('explanation.1')}
 			</p>
 			
-			<ul class="pl-4">
+			<ul class="text-sm pl-4">
 				<li>{$_('explanation.2')} <a class="link" href="https://github.com/Boavizta/environmental-footprint-data">{$_('explanation.3')}</a></li>
 				<li>{$_('explanation.4')}</li>
 				<li>{$_('explanation.5')}</li>
@@ -27,7 +27,7 @@
 	</div>
 	
 	<h3 class="title-second">{$_('index.sub-title')}</h3>
-    <div class="text-sm my-2">
+    <div class="text-sm mb-2">
         {$_('explanation.6')}
     </div>
 	<DataExplorer />
