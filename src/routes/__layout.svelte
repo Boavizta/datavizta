2f330a9fddc4e06134a183ea4e554e26679fdd5d
Gregory Pelletier <gregory.pelletier@fairness.coop>
2022-05-20 12:50:28 +0200

    add editorconfig and clean up indentation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,11 @@
 <script>
-	import '$/global.css';
-	import '$/i18n';
-	import { isLoading } from 'svelte-i18n';
-	import "../app.css";
-	import "../ext/github-ribbon.css"
+    import "$/global.css";
+    import "$/i18n";
+    import { isLoading } from "svelte-i18n";
+    import "../app.css";
+    import "../ext/github-ribbon.css";
 </script>
 
 {#if !$isLoading}
-	<slot />
+    <slot />
 {/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,40 +1,42 @@
 <script lang="ts" xmlns="http://www.w3.org/1999/html">
-	import { _ } from 'svelte-i18n';
-	import DataExplorer from "$lib/DataExplorer.svelte";
+    import { _ } from "svelte-i18n";
+    import DataExplorer from "$lib/DataExplorer.svelte";
 </script>
 
 <div id="navbar">
-	<a href="https://boavizta.org"><img id="logo" style="height: 47px;width: 32px;" src="./boavizta-logo-4.png" alt="Boavizta"></a>
-	<h1 class="title-main">Boavizta Dataviz</h1>
+    <a href="https://boavizta.org"><img id="logo" style="height: 47px;width: 32px;" src="./boavizta-logo-4.png" alt="Boavizta"></a>
+    <h1 class="title-main">Boavizta Dataviz</h1>
 
 </div>
 <div id="content" class="px-4">
-	<h2 class="title-second">{$_('index.title')}</h2>
-	
-	<div class="">
-		<div class="mb-3">
-			<p class="text-sm mb-2">
-				{$_('explanation.1')}
-			</p>
-			
-			<ul class="text-sm pl-4">
-				<li>{$_('explanation.2')} <a class="link" href="https://github.com/Boavizta/environmental-footprint-data" target="_blank">{$_('explanation.3')}</a></li>
-				<li>{$_('explanation.4')}</li>
-				<li>{$_('explanation.5')}</li>
-			</ul>
-			<p class="text-sm mb-2">
-				{$_('explanation.10')} <a class="link" href={$_('explanation.12')} target="_blank">{$_('explanation.11')}</a>
-			</p>
-		</div>
-		
-	</div>
-	
-	<h3 class="title-second">{$_('index.sub-title')}</h3>
+    <h2 class="title-second">{$_('index.title')}</h2>
+
+    <div class="">
+        <div class="mb-3">
+            <p class="text-sm mb-2">
+                {$_('explanation.1')}
+            </p>
+
+            <ul class="text-sm pl-4">
+                <li>{$_('explanation.2')} <a class="link" href="https://github.com/Boavizta/environmental-footprint-data" target="_blank">{$_('explanation.3')}</a></li>
+                <li>{$_('explanation.4')}</li>
+                <li>{$_('explanation.5')}</li>
+            </ul>
+            <p class="text-sm mb-2">
+                {$_('explanation.10')} <a class="link" href={$_('explanation.12')} target="_blank">{$_('explanation.11')}</a>
+            </p>
+        </div>
+
+    </div>
+
+    <h3 class="title-second">{$_('index.sub-title')}</h3>
     <div class="text-sm mb-2">
         {$_('explanation.6')}
     </div>
-	<DataExplorer />
 
-	
+    <h3 class="title-second">{$_("index.sub-title")}</h3>
+    <div class="text-sm mb-2">
+        {$_("explanation.6")}
+    </div>
+    <DataExplorer />
 </div>
-
