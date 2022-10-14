a30eb18c286091d8ce42046747837421da813cc0
Julien Barritaud <julienbarritaud@gmail.com>
2022-10-14 14:41:10 +0200

    add layout

--- src/routes/serversimpact/+page.svelte
+++ src/routes/serversimpact/+page.svelte
@@ -4,3 +4,17 @@
 <div>
     <h2>Calculer l'impact de ses serveurs</h2>
 </div>
+
+
+<div class="grid space-x-10 evenly-spaced grid-cols-3">
+    <div class="min-h-[200px] shadow-md p-4 m-5">
+        <h2 class="text-3xl font-bold dark:text-white">Formulaire serveur</h2>
+    </div>
+
+    <div class="min-h-[200px] shadow-md p-4 m-5">
+        <h2 class="text-3xl font-bold dark:text-white">Utilisation</h2>
+    </div>
+    <div class="min-h-[200px] shadow-md p-4 m-5">
+        <h2 class="text-3xl font-bold dark:text-white">Résultat</h2>
+    </div>
+</div>
