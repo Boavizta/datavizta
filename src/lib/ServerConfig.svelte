91279b55c30462e129f49cfb4ae8fe766a843179
Thibault Simon <thibault.simon@orange.com>
2022-10-14 15:38:38 +0200

    feat: added form for server configuration and objects definition

---
+++ src/lib/ServerConfig.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import type { ConfigurationServer } from "$lib/types/hardware";
+
+    /*Bound var*/
+    export let serverConfig: ConfigurationServer;
+</script>
+
+<form>
+    1 serveur possédant:
+
+    <input
+        bind:value={serverConfig.cpu.units}
+        class="form-control"
+        type="number"
+        min="1"
+        max="10"
+    />
+    CPU(s) de
+    <input
+        bind:value={serverConfig.cpu.core_units}
+        class="form-control"
+        type="number"
+        min="1"
+        max="100"
+    />
+    coeurs, et
+    <input
+        bind:value={serverConfig.ram[0].units}
+        class="form-control"
+        type="number"
+        min="1"
+        max="100"
+    />
+    barrettes de
+    <input
+        bind:value={serverConfig.ram[0].units}
+        class="form-control"
+        type="number"
+        min="1"
+        max="100"
+    />
+    Go de RAM.
+    <br />
+    <input
+        bind:value={serverConfig.disk[0].units}
+        class="form-control"
+        type="number"
+        min="1"
+        max="100"
+    />
+    disques
+    <select bind:value={serverConfig.disk[0].type}>
+        <option selected value="ssd"> SSD </option>
+        <option value="hdd"> HDD </option>
+        <option value="" class="form-check-input">Unknown</option>
+    </select>
+    de
+    <input
+        bind:value={serverConfig.disk[0].capacity}
+        class="form-control"
+        type="number"
+        min="1"
+        max="10000"
+    /> Go
+</form>
--- src/routes/serversimpact/+page.svelte
+++ src/routes/serversimpact/+page.svelte
@@ -1,14 +1,40 @@
-<script>
-    import { _} from "svelte-i18n";
-</script>
-<div>
-    <h2>{$_('server-impact.title')}</h2>
-</div>
+<script lang="ts">
+    import ServerConfig from "$lib/ServerConfig.svelte";
+    import type { Server } from "$lib/types/hardware";
+    import { _ } from "svelte-i18n";
 
+    let server: Server = {
+        config: {
+            cpu: {
+                units: 1,
+                core_units: 16,
+            },
+            ram: [
+                {
+                    units: 2,
+                    capacity: 16,
+                },
+            ],
+            disk: [
+                {
+                    units: 1,
+                    capacity: 512,
+                    type: "ssd",
+                },
+            ],
+            power_supply: undefined,
+        },
+    };
+</script>
+
+<div>
+    <h2>{$_("server-impact.title")}</h2>
+</div>
 
 <div class="grid space-x-10 evenly-spaced grid-cols-3">
     <div class="min-h-[200px] shadow-md p-4 m-5">
         <h2 class="text-3xl font-bold dark:text-white">Formulaire serveur</h2>
+        <ServerConfig bind:serverConfig={server.config} />
     </div>
 
     <div class="min-h-[200px] shadow-md p-4 m-5">
