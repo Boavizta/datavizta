c4ea645460cd114ca5241827d360d6beb6949633
da-ekchajzer <david_ekchajzer@Laptop.dek>
2022-11-22 09:17:38 +0100

    config form style

--- src/lib/usage/ServerUsage.svelte
+++ src/lib/usage/ServerUsage.svelte
@@ -74,7 +74,7 @@
         </div>
     </div>
     <div class="basis-1/2 my-2 mx-2" id="conso">
-        <label class="block mb-2 text-sm font-medium text-gray-900">Average electrical consumption (Watt/hour)</label>
+        <label class="block mb-2 text-sm font-medium text-gray-900">Avrg electrical consumption (Watt/hour)</label>
         <input bind:value={usage.hours_electrical_consumption} type="number" min="1" max="10" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
     </div>
     <div class="basis-1/2 my-2 mx-2" id="model" style="display: none">
