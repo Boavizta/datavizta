acbb5315217de43681cc09802a97bcaab6b36b7d
Airloren <romain@cmakers.io>
2022-11-25 22:39:26 +0100

    improve barchart style

--- src/lib/chart/HorizBarChart.svelte
+++ src/lib/chart/HorizBarChart.svelte
@@ -39,29 +39,29 @@
   }
   
 </script>
-  <h3 class="mb-2 mx-2 text-l font-bold">{criteria}</h3>
+  <h3 class="mb-2 mx-2 text-l font-bold">{criteria} ({result.unit})</h3>
 
-<div class="stacked-bar-graph">
-  <span style="width:{use_pct}%; border-top-left-radius: 3px" class="bar-2"></span>
-  <span style="width:{fab_pct}%; border-top-right-radius: 3px" class="bar-1"></span>
+<div class="stacked-bar-graph rounded-top">
+  <span style="width:{use_pct}%" class="bar-2"></span>
+  <span style="width:{fab_pct}%" class="bar-1"></span>
 </div>
-<div class="stacked-bar-graph">
-  <span style="width:{use_pct}%; border-bottom-left-radius: 3px" class="bar-2"></span>
+<div class="stacked-bar-graph rounded-bottom">
+  <span style="width:{use_pct}%" class="bar-2"></span>
   <span style="width:{ram_pct}%" class="bar-3"></span>
   <span style="width:{cpu_pct}%" class="bar-4"></span>
   <span style="width:{ssd_pct}%" class="bar-5"></span>
   <span style="width:{hdd_pct}%" class="bar-6"></span>
-  <span style="width:{others_pct}%; border-bottom-right-radius: 3px" class="bar-7"></span>
+  <span style="width:{others_pct}%" class="bar-7"></span>
 </div>
 <div class="px-4 text-xs w-full">
-  <span class="bar-2 bar-legend"></span> {$_('server-impact.Usage')} : {result.use}{result.unit}
-  <span class="bar-1 bar-legend"></span> {$_('server-impact.Manufacture')} : {result.use}{result.unit}
+  <span class="bar-2 bar-legend"></span>{$_('server-impact.Usage')} : {result.use}
+  <span class="bar-1 bar-legend ml-2"></span> {$_('server-impact.Manufacture')} : {result.use}
 </div>
 <div class="px-4 pb-4 text-xs w-full">
-  <span class="bar-3 bar-legend"></span> {$_('server-impact.Manufacture')} RAM : {result.ram}{result.unit}  
-  <span class="bar-4 bar-legend"></span> {$_('server-impact.Manufacture')} CPU : {result.cpu}{result.unit}  
-  <span class="bar-5 bar-legend"></span> {$_('server-impact.Manufacture')} SSD : {result.ssd}{result.unit}  
-  <span class="bar-6 bar-legend"></span> {$_('server-impact.Manufacture')} HDD : {result.hdd}{result.unit}
-  <span class="bar-7 bar-legend"></span> {$_('server-impact.Manufacture')} {$_('server-impact.Others')} : {result_others}{result.unit}
+  <span class="bar-3 bar-legend"></span> {$_('server-impact.Manufacture')} RAM : {result.ram}
+  <span class="bar-4 bar-legend ml-2"></span> {$_('server-impact.Manufacture')} CPU : {result.cpu}
+  <span class="bar-5 bar-legend ml-2"></span> {$_('server-impact.Manufacture')} SSD : {result.ssd}
+  <span class="bar-6 bar-legend ml-2"></span> {$_('server-impact.Manufacture')} HDD : {result.hdd}
+  <span class="bar-7 bar-legend ml-2"></span> {$_('server-impact.Manufacture')} {$_('server-impact.Others')} : {result_others}
 </div>
 
