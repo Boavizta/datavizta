d58b3b9015bb44d22b53bcc0896c6ea1e8a8fffb
olivier <odelcroi@gmail.com>
2022-11-28 23:37:52 +0100

    move to spa

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,3 @@
-export const prerender = true;
+// This can be false if you're using a fallback (i.e. SPA mode)
+export const prerender = false;//configure spa
+export const ssr = false;//configure spa for dev https://github.com/sveltejs/kit/tree/master/packages/adapter-static#turn-off-ssr
--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		//https://github.com/sveltejs/kit/tree/master/packages/adapter-static#spa-mode
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
+			//pages: 'build',
+			//assets: 'build',
+			fallback: 'index.html',//configure spa
+			//precompress: false
 		}),
+		prerender: { entries: [] }
 /* 		
 https://github.com/sveltejs/kit/pull/6197
 prerender: {
