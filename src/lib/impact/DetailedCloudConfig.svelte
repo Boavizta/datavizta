f4cd624e9a7ead78d6f9fae9a5e2aa1e03be7e65
Airloren <romain@cmakers.io>
2022-12-21 16:43:57 +0100

    fix ssd and hdd details

--- src/lib/impact/DetailedCloudConfig.svelte
+++ src/lib/impact/DetailedCloudConfig.svelte
@@ -16,18 +16,22 @@
     <div class="ml-4 px-1 w-full">
         <p><span class="text-xs"> {loadedImpact['verbose']["CPU-1"]["units"]} {loadedImpact['verbose']["CPU-1"]["model_range"]["value"]} CPU {$_('with')} TDP {loadedImpact['verbose']["CPU-1"]["tdp"]["value"]}{loadedImpact['verbose']["CPU-1"]["tdp"]["unit"]} {$_('and')} {loadedImpact['verbose']["CPU-1"]["core_units"]["value"]} {$_('cores')} (die size: {loadedImpact['verbose']["CPU-1"]["die_size_per_core"]["value"]}{loadedImpact['verbose']["CPU-1"]["die_size_per_core"]["unit"]})</span></p>
         <p><span class="text-xs"> {loadedImpact['verbose']["RAM-1"]["units"]} x {loadedImpact['verbose']["RAM-1"]["capacity"]["value"]}{loadedImpact['verbose']["RAM-1"]["capacity"]["unit"]} RAM {$_('with')} {loadedImpact['verbose']["RAM-1"]["density"]["value"]}{loadedImpact['verbose']["RAM-1"]["density"]["unit"]} {$_('density')}</span></p>
-        {#if (loadedImpact['verbose']["HDD-1"] != undefined)}
-            {#if (loadedImpact['verbose']["HDD-1"]["units"] > 0)}
+        {#if (loadedImpact['verbose']["SSD-1"] != undefined)}
+            {#if (loadedImpact['verbose']["SSD-1"]["units"] > 0)}
             <p><span class="text-xs"> {loadedImpact['verbose']["SSD-1"]["units"]} x {loadedImpact['verbose']["SSD-1"]["capacity"]["value"]}{loadedImpact['verbose']["SSD-1"]["capacity"]["unit"]} SSD {$_('with')} {loadedImpact['verbose']["SSD-1"]["density"]["value"]}{loadedImpact['verbose']["SSD-1"]["density"]["unit"]} {$_('density')}</span></p>
-            {/if}
             {:else}
+            <p><span class="text-xs">{$_('without')} SSD</span></p>
+            {/if}
+        {:else}
             <p><span class="text-xs">{$_('without')} SSD</span></p>
         {/if}
         {#if (loadedImpact['verbose']["HDD-1"] != undefined)}
             {#if (loadedImpact['verbose']["HDD-1"]["units"] > 0)}
-                <p><span class="text-xs"> {loadedImpact['verbose']["HDD-1"]["units"]} x {loadedImpact['verbose']["HDD-1"]["capacity"]["value"]}{loadedImpact['verbose']["HDD-1"]["capacity"]["unit"]} HDD </span></p>
-            {/if}
+                <p><span class="text-xs"> {loadedImpact['verbose']["HDD-1"]["units"]} x HDD </span></p>
             {:else}
+            <p><span class="text-xs">{$_('without')} HDD</span></p>
+            {/if}
+        {:else}
             <p><span class="text-xs">{$_('without')} HDD</span></p>
         {/if}
     </div>
