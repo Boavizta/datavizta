413b0d22c13dfb351aacbfaeee157da8c03ff799
Airloren <romain@cmakers.io>
2023-06-30 10:30:44 +0200

    fix elec factor issue

--- src/lib/impact/usageconfig/DetailedUsageConfig.svelte
+++ src/lib/impact/usageconfig/DetailedUsageConfig.svelte
@@ -14,44 +14,44 @@
     <div class="ml-4 px-1 w-full">
         <p><a class="text-xs">{$_('detailed-config.usage-conso')}{loadedImpact['verbose']["avg_power"]["value"]}{loadedImpact['verbose']["avg_power"]["unit"]}</a></p>
         <p><a class="text-xs">{$_('detailed-config.usage-title')}</a></p>
-        {#if "adpe" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["adpe"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">ADPE:{loadedImpact['impacts']["adpe"]["use"]["value"]}{loadedImpact['impacts']["adpe"]["unit"]}</span></p>
+        {#if "adpe_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["adpe_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">ADPE:{loadedImpact['verbose']["adpe_factor"]["value"]}{loadedImpact['verbose']["adpe_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "adp" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["adp"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">ADP:{loadedImpact['impacts']["adp"]["use"]["value"]}{loadedImpact['impacts']["adp"]["unit"]}</span></p>
+        {#if "adp_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["adp_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">ADP:{loadedImpact['verbose']["adp_factor"]["value"]}{loadedImpact['verbose']["adp_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "gwp" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["gwp"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">GWP:{loadedImpact['impacts']["gwp"]["use"]["value"]}{loadedImpact['impacts']["gwp"]["unit"]}</span></p>
+        {#if "gwp_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["gwp_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">GWP:{loadedImpact['verbose']["gwp_factor"]["value"]}{loadedImpact['verbose']["gwp_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "pe" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["pe"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">PE:{loadedImpact['impacts']["pe"]["use"]["value"]}{loadedImpact['impacts']["pe"]["unit"]}</span></p>
+        {#if "pe_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["pe_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">PE:{loadedImpact['verbose']["pe_factor"]["value"]}{loadedImpact['verbose']["pe_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "ir" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["ir"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">IR:{loadedImpact['impacts']["ir"]["use"]["value"]}{loadedImpact['impacts']["ir"]["unit"]}</span></p>
+        {#if "ir_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["ir_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">IR:{loadedImpact['verbose']["ir_factor"]["value"]}{loadedImpact['verbose']["ir_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "odp" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["odp"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">ODP:{loadedImpact['impacts']["odp"]["use"]["value"]}{loadedImpact['impacts']["odp"]["unit"]}</span></p>
+        {#if "odp_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["odp_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">ODP:{loadedImpact['verbose']["odp_factor"]["value"]}{loadedImpact['verbose']["odp_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "ap" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["ap"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">AP:{loadedImpact['impacts']["ap"]["use"]["value"]}{loadedImpact['impacts']["ap"]["unit"]}</span></p>
+        {#if "ap_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["ap_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">AP:{loadedImpact['verbose']["ap_factor"]["value"]}{loadedImpact['verbose']["ap_factor"]["unit"]}</span></p>
         {/if}
         {/if}
-        {#if "ept" in loadedImpact['impacts']}
-        {#if loadedImpact['impacts']["ept"]["use"]["value"]!= undefined}
-        <p><span class="ml-5 text-xs">IR:{loadedImpact['impacts']["ept"]["use"]["value"]}{loadedImpact['impacts']["ept"]["unit"]}</span></p>
+        {#if "ept_factor" in loadedImpact['verbose']}
+        {#if loadedImpact['verbose']["ept_factor"]["value"]!= undefined}
+        <p><span class="ml-5 text-xs">IR:{loadedImpact['verbose']["ept_factor"]["value"]}{loadedImpact['verbose']["ept_factor"]["unit"]}</span></p>
         {/if}
         {/if}
     </div>
