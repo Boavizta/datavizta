ce6edbbb02006327a5c5781c9dd6e7efac348c81
Yanis Riani <Yann.riani@gmail.com>
2023-11-20 10:48:06 +0100

    Refactor - Optimization  and css

--- src/lib/impact/iot/IoTConfig.svelte
+++ src/lib/impact/iot/IoTConfig.svelte
@@ -7,70 +7,8 @@
 
     export let IoTConfig: IoT;
 
-    let iot_hsl = [];
-    let selectedFunctionalBlockIndex = 0;
-
-    function getitems(route) {
-        return get(route)
-            .then((response) => response.json())
-            .then((data) => {
-                let elements = [];
-                for (let i = 0; i < data.length; i++) {
-                    elements.push({ value: data[i], label: data[i] });
-                }
-                return elements;
-            });
-    }
-
-    function getfirstitem(route) {
-        return get(route)
-            .then((response) => response.json())
-            .then((data) => {
-                return data[0];
-            });
-    }
-
-    onMount(async () => {
-        IoTConfig.functional_blocks[selectedFunctionalBlockIndex].type =
-            iot_fonctional_blocks[0].type;
-        IoTConfig.functional_blocks[selectedFunctionalBlockIndex].hsl_level =
-            iot_fonctional_blocks[0].hsl_levels[0];
-        iot_hsl = iot_fonctional_blocks[0].hsl_levels;
-    });
-
-    async function fonctional_blocks_type_select(event) {
-        if (event.detail.index === undefined) return;
-
-        IoTConfig.functional_blocks[selectedFunctionalBlockIndex].type =
-            iot_fonctional_blocks[event.detail.index].type;
-        IoTConfig.functional_blocks[selectedFunctionalBlockIndex].hsl_level =
-            iot_fonctional_blocks[event.detail.index].hsl_levels[0];
-
-        iot_hsl = iot_fonctional_blocks[event.detail.index].hsl_levels;
-    }
-
-    function fonctional_blocks_hsl_select(event) {
-        if (event.detail.value === undefined) return;
-
-        IoTConfig.functional_blocks[selectedFunctionalBlockIndex].hsl_level =
-            event.detail.value;
-    }
-
-    function archetype_select(event) {
-        if (event.detail.value === undefined) return;
-
-        IoTConfig.archetype = event.detail.value;
-    }
-
-    const iot_archetype = [
-        "occupancy_sensor",
-        "home_connected_assistant_mini",
-        "drone_mini",
-        "smart_watch",
-        "iot-device-default",
-    ];
-
-    const iot_fonctional_blocks = [
+    let iot_archetype = [];
+    const iot_functional_blocks = [
         {
             type: "Actuators",
             hsl_levels: ["HSL-1", "HSL-2", "HSL-3"],
@@ -116,91 +54,128 @@
             hsl_levels: ["HSL-1", "HSL-2", "HSL-3"],
         },
     ];
-    function addFunctionalBlock() {
-        const newBlock = {
-            hsl_level: "HSL-1",
-            type: "security",
+
+    function getitems(route) {
+        return get(route)
+            .then((response) => response.json())
+            .then((data) => {
+                let elements = [];
+                for (let i = 0; i < data.length; i++) {
+                    elements.push({ value: data[i], label: data[i] });
+                }
+                return elements;
+            });
+    }
+
+    function getfirstitem(route) {
+        return get(route)
+            .then((response) => response.json())
+            .then((data) => {
+                return data[0];
+            });
+    }
+
+    function fetchArchetypes() {
+        return get("iot/iot_device/archetypes")
+            .then((response) => response.json())
+            .then((data) => (iot_archetype = data));
+    }
+
+    function getHslLevelsForType(type) {
+        console.log("getHslLevelsForType", type);
+        const block = iot_functional_blocks.find(
+            (block) => block.type === type
+        );
+        return block ? block.hsl_levels : [];
+    }
+
+    function updateFunctionalBlock(type, hsl_level, index) {
+        const updatedBlock = {
+            type,
+            hsl_level: hsl_level || getHslLevelsForType(type)[0],
         };
-        IoTConfig.functional_blocks = [
-            ...IoTConfig.functional_blocks,
-            newBlock,
-        ];
-        selectedFunctionalBlockIndex = IoTConfig.functional_blocks.length - 1;
+        IoTConfig.functional_blocks[index] = updatedBlock;
+        IoTConfig.functional_blocks = [...IoTConfig.functional_blocks];
+    }
+
+    function functionalBlocksTypeSelect({ detail }, index) {
+        const type = detail.value;
+        const hsl_levels = getHslLevelsForType(type);
+        updateFunctionalBlock(type, hsl_levels[0], index);
+    }
+
+    function addFunctionalBlock() {
+        updateFunctionalBlock(
+            "Actuators",
+            "HSL-1",
+            IoTConfig.functional_blocks.length
+        );
     }
 
     function removeFunctionalBlock(index) {
-        if (IoTConfig.functional_blocks.length > 1) {
-            const updatedBlocks = IoTConfig.functional_blocks.filter(
-                (_, i) => i !== index
-            );
-            IoTConfig.functional_blocks = updatedBlocks;
-            if (selectedFunctionalBlockIndex >= updatedBlocks.length) {
-                selectedFunctionalBlockIndex = updatedBlocks.length - 1;
-            }
-        }
+        IoTConfig.functional_blocks = IoTConfig.functional_blocks.filter(
+            (_, i) => i !== index
+        );
     }
+
+    onMount(fetchArchetypes);
 </script>
 
 <div
     class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4"
 >
-    <div class="flex justify-center w-full">
-        <div class="w-full max-w-xs">
-            <label class="block text-sm font-medium text-gray-900">
-                {$_("iot-config.archetype")}
-            </label>
+    <div class="flex justify-center w-full mb-4">
+        <div class="w-full">
+            <label class="block text-sm font-medium text-gray-900"
+                >{$_("iot-config.archetype")}</label
+            >
             <Select
                 items={iot_archetype}
-                on:select={archetype_select}
+                on:select={({ detail }) => (IoTConfig.archetype = detail.value)}
                 value={iot_archetype[0]}
             />
         </div>
     </div>
 
     <div class="flex justify-start mb-4">
-        <h3 class="mb-2 mx-2 text-2xl font-bold">
-            {$_("iot-config.fonctional_blocks")}
-        </h3>
+        <h3 class="text-xl my-1">{$_("iot-config.functional_blocks")}</h3>
         <img
             on:click={addFunctionalBlock}
             src="./src/routes/iotimpact/plus-icon.svg"
-            alt="add icon"
-            class="cursor-pointer hover:opacity-70"
+            alt="Add icon"
+            class="cursor-pointer hover:opacity-70 ml-4"
         />
     </div>
 
     {#each IoTConfig.functional_blocks as block, index (block)}
-        <div class="mb-4">
-            <div class="flex items-center mb-2">
-                <div class="w-1/2 mr-2">
-                    <label class="block text-sm font-medium text-gray-900">
-                        {$_("iot-config.type")}
-                    </label>
-                    <Select
-                        items={iot_fonctional_blocks.map((obj) => obj.type)}
-                        on:select={fonctional_blocks_type_select}
-                        value={IoTConfig.functional_blocks[index].type}
-                    />
-                </div>
-                <div class="w-1/2 ml-2">
-                    <label class="block text-sm font-medium text-gray-900">
-                        {$_("iot-config.hsl")}
-                    </label>
-                    <Select
-                        items={iot_hsl}
-                        on:select={fonctional_blocks_hsl_select}
-                        value={IoTConfig.functional_blocks[index].hsl_level}
-                    />
-                </div>
-                <div class="ml-4">
-                    <img
-                        on:click={() => removeFunctionalBlock(index)}
-                        src="./src/routes/iotimpact/trash-icon.svg"
-                        alt="delete icon"
-                        class="mt-2 cursor-pointer hover:opacity-70"
-                    />
-                </div>
+        <div class="flex items-center mb-4">
+            <div class="w-1/2 mr-2">
+                <label class="block text-sm font-medium text-gray-900">
+                    {$_("iot-config.type")}
+                </label>
+                <Select
+                    items={iot_functional_blocks.map((obj) => obj.type)}
+                    on:select={(e) => functionalBlocksTypeSelect(e, index)}
+                    value={block.type}
+                />
             </div>
+            <div class="w-1/2 ml-2">
+                <label class="block text-sm font-medium text-gray-900">
+                    {$_("iot-config.hsl")}
+                </label>
+                <Select
+                    items={getHslLevelsForType(block.type)}
+                    on:select={({ detail }) =>
+                        updateFunctionalBlock(block.type, detail.value, index)}
+                    value={block.hsl_level}
+                />
+            </div>
+            <img
+                on:click={() => removeFunctionalBlock(index)}
+                src="./src/routes/iotimpact/delete-icon.svg"
+                alt="delete icon"
+                class="cursor-pointer hover:opacity-70 ml-4 mt-5"
+            />
         </div>
     {/each}
 </div>
--- src/routes/iotimpact/+page.svelte
+++ src/routes/iotimpact/+page.svelte
@@ -43,7 +43,7 @@
     function createIoT(): IoT {
         return {
             archetype: "occupancy_sensor",
-            functional_blocks: [{ hsl_level: "HSL-1", type: "security" }],
+            functional_blocks: [{ type: "Actuators", hsl_level: "HSL-1" }],
             usage: {
                 avg_power: 150,
                 use_time_ratio: 1,
